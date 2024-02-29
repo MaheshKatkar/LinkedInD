15de24009a61c7eceb5b9e88bd42394458f2df74
Mahesh katkar <124710097+MaheshKatkar@users.noreply.github.com>
2024-02-29 19:23:28 +0530

    initial commit for backend

---
+++ BackEnd/LinkedInD/src/main/java/com/demo/LinkedInD/LinkedInDApplication.java
@@ -0,0 +1,13 @@
+package com.demo.LinkedInD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInDApplication.class, args);
+	}
+
+}
---
+++ BackEnd/LinkedInD/src/test/java/com/demo/LinkedInD/LinkedInDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.LinkedInD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
